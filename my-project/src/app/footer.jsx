49863eac81f09c24972faace08101e4cf28a3cac
hnifalii <ankso30561@gmail.com>
2024-07-05 19:45:19 +0700

    added env

--- my-project/src/app/footer.jsx
+++ my-project/src/app/footer.jsx
@@ -8,17 +8,17 @@ import { FaWhatsapp } from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-      <footer className="flex flex-wrap mt-20 bottom-0 bg-primary text-white gap-[180px] justify-center">
+      <footer className="flex w-screen mt-20 bg-primary text-white gap-[180px] justify-center">
         <div className="p-8">
-          <h2 className="text-4xl mb-6 lg:text-[48px] md:text-[48px] font-bold hover:cursor-default">earnit</h2>
-          <p className="hover:cursor-default">Social Media</p>
+          <h2 className="text-4xl mb-6 font-bold hover:cursor-default">earnit</h2>
+          <p className="text-sm hover:cursor-default">Social Media</p>
           <div className="flex flex-col gap-2 pt-2">
             <div className="flex flex-row items-center">
-              <MdOutlineMailOutline className="size-8 mr-2" />
+              <MdOutlineMailOutline className="size-6 mr-2" />
               <p>earnit@business.inq</p>
             </div>
             <div className="flex flex-row items-center">
-              <FaWhatsapp className="size-8 mr-2" />
+              <FaWhatsapp className="size-6 mr-2" />
               <p>086969696969</p>
             </div>
             <div className="flex flex-row items-center">
@@ -28,11 +28,11 @@ const Footer = () => {
         </div>
 
         <div className="p-8 pt-12 align-center">
-          <h5 className="text-lg font-semibold pb-1 hover:cursor-default">
+          <h5 className="text-sm font-semibold pb-1 hover:cursor-default">
             Customer Service
           </h5>
-          <div className="bg-white w-36 h-[2px] my-1"></div>
-          <div className="flex flex-col gap-2 mt-2">
+          <div className="bg-white w-30 h-[2px] my-1"></div>
+          <div className="flex flex-col gap-2 mt-2 text-xs">
             <a href="about.html">Tentang kami</a>
             <a href="#">FAQ</a>
             <a href="#">Syarat dan Ketentuan</a>
